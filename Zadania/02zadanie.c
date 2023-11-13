ebf028b7d3485cff86f5ee628b1eeffa5e154924
aleksalut <148879741+aleksalut@users.noreply.github.com>
2023-11-13 20:42:45 +0000

    Zadanie2 was added

---
+++ Zadania/02zadanie.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void) {
+  char char1 = 'A';
+  char char2 = 'B';
+
+  int int1 = 18;
+  int int2 = 27;
+
+  float float1 = 21.37;
+  float float2 = 6.2;
+
+  double double1 = 4.7881;
+  double double2 = 5.800700;
+
+  printf("char1: %c\n" , char1);
+  printf("char2: %c\n" , char2);
+
+  printf("%d\n" , int1);
+  printf("%d\n" , int2);
+
+  printf("%f\n" , float1);
+  printf("%f\n", float2);
+
+  printf("%lf\n" , double1);
+  printf("%lf\n" , double2);
+
+
+
+
+
+  return 0;
+}
